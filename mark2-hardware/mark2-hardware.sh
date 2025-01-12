28ddd2fb2d7e8b3c73e1e8149281cc21951d31a2
andlo <andlo@outlook.dk>
2025-01-12 12:52:16 +0100

    ...

--- mark2-hardware/mark2-hardware.sh
+++ mark2-hardware/mark2-hardware.sh
@@ -21,7 +21,7 @@ if [ "$KERNEL_VERSION" != "$LAST_KERNEL_VERSION" ]; then
     # Update and install necessary packages
     echo "Updating and installing necessary packages..."
     apt-get update
-    apt-get install -y git cmake build-essential raspberrypi-kernel-headers jq python3-dev
+    apt-get install -y git cmake build-essential raspberrypi-kernel-headers jq python3-dev python3-venv python3-pip
 
     # Enable I2C interface
     echo "Enabling I2C interface..."
